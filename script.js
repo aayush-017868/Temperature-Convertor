a2478842438a074db5c027a1dce14e07c64f9ca7
aayush-017868 <119842199+aayush-017868@users.noreply.github.com>
2024-03-04 12:52:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,87 @@
 const calculateTemp = () => {
-    const inputTemp = document.getElementById('tempretur').value;
+    //The value entered will be stored in this variable
+    const inputTemp = document.getElementById('tempreture').value;
+    //The Option selected will be stored into this variable
+    const tempSelectedFrom = document.getElementById('tempFrom');
+    const tempSelectedTo = document.getElementById('tempTo');
+    
+    const valueTempFrom = tempSelectedFrom.options[tempSelectedFrom.selectedIndex].value;
+    const valueTempTo = tempSelectedTo.options[tempSelectedTo.selectedIndex].value;
 
-    const tempSelected = document.getElementById('temprut');
-    const valueTemp = temprut.options[tempSelected.selectedIndex].value;
 
+    const cal = (valueTempFrom) => {
+        if(valueTempFrom == 'cel') return "Celcius";
+        if(valueTempFrom == 'fah') return "Fahrenheit";
+        if(valueTempFrom == 'kel') return "Kelvin";
+    };
+
+    //Converting Celcius into fahrenheit
     const celtofa = (cel) => {
-        let fahrenheit = ((cel* 9 / 5) + 32).toFixed(1);
-        return fahrenheit;
+        return ((cel* 9 / 5) + 32).toFixed(1);
     }
 
-
-    const farntocel= (fah) => {
-        let celsius = ((fah- 32) * 5 / 9).toFixed(1);
-        return celsius;
+    //Converting Celcius into Kelvin
+    const celtokel = (cel) => {
+        return (273.15+parseInt(cel));
+    }
+    //Converting Kelvin into Celcius
+    const keltocel = (kel) => {
+        return (kel - 273.15).toFixed(1);
+    }
+    //Converting fahrenheit into Celcius
+    const fatocel= (fah) => {
+        return ((fah- 32) * 5 / 9).toFixed(1);
+    }
+    //Converting Kelvin into fahrenheit
+    const keltofa = (kel) =>{
+        return celtofa(keltocel(kel)); 
+    }
+    //Converting fahrenheit into Kelvin
+    const fatokel = (fah) => {
+        return celtokel(fatocel(fah));
     }
 
-    if (valueTemp == 'cel') {
-        document.getElementById("res").innerHTML = celtofa(inputTemp) + " Fahrenheit";
+    //Converting Celcius into fahrenheit
+    if(valueTempFrom == 'cel' && valueTempTo == 'fah') {
+        document.getElementById("res1").innerHTML = celtofa(inputTemp) + " Fahrenheit";
     }
-    else {
-        document.getElementById("res").innerHTML = farntocel(inputTemp) + " Celsius";
+    //Converting Celcius into Kelvin
+    else if(valueTempFrom == 'cel' && valueTempTo == 'kel'){
+        document.getElementById("res1").innerHTML = celtokel(inputTemp) + " Kelvin";
     }
+     //Converting fahrenheit into Celcius
+    else if(valueTempFrom == 'fah' && valueTempTo == 'cel'){
+        document.getElementById("res1").innerHTML = fatocel(inputTemp) + " Celsius";
+    }
+    //Converting fahrenheit into Kelvin
+    else if(valueTempFrom == 'fah' && valueTempTo == 'kel'){
+        document.getElementById("res1").innerHTML = fatokel(inputTemp) + " Kelvin";
+    }
+    //Converting Kelvin into Celcius
+    else if(valueTempFrom == 'kel' && valueTempTo == 'cel'){
+        document.getElementById("res1").innerHTML = keltocel(inputTemp) + " Celcius";
+    }
+    //Converting Kelvin into fahrenheit
+    else if(valueTempFrom == 'kel' && valueTempTo == 'fah'){
+        document.getElementById("res1").innerHTML = keltofa(inputTemp) + " Fahrenheit";
+    }
+    else{
+        document.getElementById("res1").innerHTML = inputTemp + " " + cal(valueTempFrom);
+    }
+}
+
+
+//Array for pre-defined colors
+const temperatureColors = {
+    hot: "#040106, #498cd9",
+    cold: "faf7f7, #07afe8",
+    normal: "#e2d7d7, #a9e6fb"
+  };
+//Background color change for temperature ranges
+if((inputTemp <= 10 && tempSelectedFrom == 'cel') || (inputTemp <= celtofa(10) && tempSelectedFrom == 'fah') || inputTemp <= celtokel(10) && tempSelectedFrom == 'kel'){
+    // document.body.style.backgroundColor = temperatureColors[1];
+    alert('Hello');
+}
+else {
+    document.getElementById("res1").innerHTML = "Invalid";
 }
